8d5607e8f4dc38115c9c16389cc72be9407f3558
omarpruebas <pruebasomar13@gmail.com>
2026-03-24 13:21:43 -0500

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,1 +1,7 @@
-
+export default function handler(req, res) {
+  res.status(200).json({
+    message: "Hola desde Node.js en Vercel 🚀",
+    method: req.method,
+    timestamp: new Date().toISOString()
+  });
+}
